af2bfb8904aef762f90d21c8001c9e82212cbee2
Hoybaby <mbartek436@gmail.com>
2022-03-01 17:22:00 -0500

    the route works and everything is slowly coming along

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
 
 const port = process.env.PORT || 3000;
 
+// middleware
+// if we dont do this, we wont have access to req.body
+app.use(express.json());
+
+
 // routes
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+
+// this is must to use routes. Need to make sure to get middleware from express 
+app.use('/api/vi/tasks', tasks);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}...`);

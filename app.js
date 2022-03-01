f635989477a79be2189ba91daf97180732fcc3c8
Hoybaby <mbartek436@gmail.com>
2022-03-01 17:03:15 -0500

    just adding the simple route

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}...`);
+});

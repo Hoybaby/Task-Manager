ed95d9782e58c3076f01f54dd20ed2f981263efe
Hoybaby <mbartek436@gmail.com>
2022-03-07 18:43:53 -0500

    added a file in the controller file to handle the getAllTask method that will eventually show onto the page

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 //will import controllers latrer
 
-router.route('/').get((req, res) => {
-    res.send('all items');
-})
+// accesing the controlers i made in the controllers folder
+// I am doing this so I dont have to hard corde the routers
+const {getAllTasks} = require('../controllers/tasks');
+
+router.route('/').get(getAllTasks);
 
 module.exports = router;

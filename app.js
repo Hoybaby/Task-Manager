5b51e916275e4c3f5a604338a2708e493c8ce378
Hoybaby <mbartek436@gmail.com>
2022-03-10 09:55:58 -0500

    now the server abd database are launched together under the same message

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-require('./db/connect');
+
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
-
-const port = process.env.PORT || 3000;
+const connectDB = require('./db/connect');
 
 // middleware
 // if we dont do this, we wont have access to req.body
 app.use(express.json());
-
-
 // routes
-
 // this is must to use routes. Need to make sure to get middleware from express 
 app.use('/api/v1/tasks', tasks);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}...`);
-});
+const port = process.env.PORT || 3000;
+
+// since i am returning a promise in the connect.js, i can use the await keyword.
+const start = async () => {
+    try {
+        await connectDB();
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}...`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}...`);
+// });
--- db/connect.js
+++ db/connect.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 
 const connectionString = `mongodb+srv://hoybaby:Welcome0@firstcluster.b4kva.mongodb.net/TaskManager?retryWrites=true&w=majority`;
 
+// refactoring the code where the server and database are launched at the same time and checking if the connection is successful.
+const connectDB = (url) => {
+    return mongoose.connect(connectionString, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        useFindAndModify: false, 
+        useCreateIndex: true})
+        
+}
 
-mongoose
-.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
-.then(() => console.log('Connected to database...'))
-.catch(err => console.log(err));
+module.exports = connectDB;
